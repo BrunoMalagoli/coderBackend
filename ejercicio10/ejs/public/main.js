0002c5389b5ea37ffc1ca8cce74ff8b035f6ef3c
Bruno Malagoli <bmalagoli2@gmail.com>
2022-05-13 13:48:31 -0300

    Ejercicio 22

--- ejercicio10/ejs/public/main.js
+++ ejercicio10/ejs/public/main.js
@@ -1,51 +1,73 @@
-const socket = io.connect()
-socket.on("mensaje", (data)=>{
-    console.log(data)
-})
-function render(data){
-    let html = data.map((producto)=>{
-        return `
-        <tr>
-        <th>${producto.id}</td>
-        <td>${producto.title}</td>
-        <td>${producto.price}</td>
-        <td>${producto.thumbnail}</td>
-        </tr><br>`
-    }).join(" ")
-    document.getElementById("table-body").innerHTML=  html
-}
-function renderMessages(data){
-    let chat = data.map((mensaje)=>{
-        return `
-        <span><b>${mensaje.user}</b> (${mensaje.timestamp}): ${mensaje.mensaje}</span><br>
-        ` 
-    }).join(" ")
-    document.getElementById("chat").innerHTML= chat
-}
-socket.on("productosDefault", ({productos, mensajes})=>{
-    render(productos)
-    renderMessages(mensajes)
-})
-socket.on("productosNew", (data)=>{
-    render(data)
-})
-socket.on("messageNew", (data)=>{
-    renderMessages(data)
-})
-function newProduct(){
-    const producto = {
-        title: document.getElementById("title").value,
-        price: document.getElementById("price").value,
-        thumbnail: document.getElementById("image").value
-    }
-    socket.emit("new-product", producto)
-    return false
-}
+const socket = io.connect();
+//const { denormalize } = require("normalizr");
+//const { msjSchema } = require("../server");
+//Products
 
-function newMessage(){
+function render(data) {
+  let html = data
+    .map((producto) => {
+      return `
+        <div>
+        <p>${producto.name}</p>
+        <p>${producto.price}</p>
+        <p>${producto.thumbnail}</p>
+        </div><br>`;
+    })
+    .join(" ");
+  document.getElementById("tableBody").innerHTML = html;
+}
+socket.on("initialProducts", (data) => {
+  render(data);
+});
+socket.on("initialMessages"),
+  (data) => {
+    const denormalizado = denormalize(data.result, msjSchema, data.entities);
+    console.log(denormalizado);
+    renderMessages(denormalizado);
+  };
+//Messages
+function renderMessages(data) {
+  let timestamp = Date.now();
+  let chat = data
+    .map((mensaje) => {
+      return `
+        <span><b>${mensaje.author.email}</b> (${timestamp}): ${mensaje.text}</span><br>
+        `;
+    })
+    .join(" ");
+  document.getElementById("chat").innerHTML = chat;
+}
+socket.on("messageNew", (data) => {
+  const denormalizado = denormalize(data.result, msjSchema, data.entities);
+  console.log(denormalizado);
+  renderMessages(denormalizado);
+});
+
+//Utiles
+
+let mensajes = [];
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+//Funcion submit
+async function newMessage() {
+  try {
     const mensaje = {
-        user: document.getElementById("usuario").value,
-        mensaje: document.getElementById("mensaje").value
-    }
-    socket.emit("new-message", mensaje)
+      author: {
+        email: await document.getElementById("mail").value,
+        name: await document.getElementById("name").value,
+        lname: await document.getElementById("lname").value,
+        age: await document.getElementById("age").value,
+        alias: await document.getElementById("alias").value,
+        avatar: await document.getElementById("image").value,
+      },
+      text: await document.getElementById("msj").value,
+    };
+    mensajes.push(mensaje);
+    socket.emit("new-message", mensajes);
+  } catch (e) {
+    console.log(e);
+  }
 }
--- ejercicio10/ejs/server.js
+++ ejercicio10/ejs/server.js
@@ -1,90 +1,147 @@
 const express = require("express");
 const PORT = 8080;
-const {Server : IOServer}= require("socket.io")
-const {Server : HttpServer} = require("http");
+const { Server: IOServer } = require("socket.io");
+const { Server: HttpServer } = require("http");
+const faker = require("faker");
 const app = express();
-const http = new HttpServer(app)
-const io = new IOServer(http)
+const http = new HttpServer(app);
+const io = new IOServer(http);
 const axios = require("axios");
+const mongoose = require("mongoose");
+const URL = require("../.env/dbOpt");
+const db = require("./MDBSchemas/messSchema");
+const { schema, normalize, denormalize } = require("normalizr");
+const util = require("util");
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const Contenedor = require("./public/api");
-const sqliteOPT = require("../options/sqlite");
-const mariaOPT = require("../options/mariaDB")
-const contenedorProductos = new Contenedor(mariaOPT, "productos")
-const contenedorMensajes = new Contenedor(sqliteOPT, "mensajes")
-
+let productos = [];
+let mensajes;
 //Func
-const defaultData = async (socket) =>{
-  try{
-  const productos = await axios.get("http://localhost:8080/producto").then((productos)=>{
-    return productos.data
-  });
-  const mensajes = await axios.get("http://localhost:8080/mensaje").then((msj)=>{
-    console.log(msj.data)
-    return msj.data
-  });
-    socket.emit("productosDefault", {
-      productos,
-      mensajes
-    })
-  }catch(e){
-    console.log(e)
+async function getDataProducts() {
+  try {
+    let response;
+    await axios.get("http://localhost:8080/api/productos-test").then((res) => {
+      response = res.data;
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
   }
+}
+async function getDataMessages() {
+  try {
+    let response;
+    await axios.get("http://localhost:8080/api/mensaje").then((res) => {
+      response = res.data;
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function mensajesIniciales() {
+  try {
+    let initialMjs = await getDataMessages();
+    return (mensajes = initialMjs);
+  } catch (e) {
+    console.log(e);
+  }
+}
+function print(objeto) {
+  console.log(util.inspect(objeto, false, 12, true));
+}
+function compression(unnormalized, normalized) {
+  let division = normalized / unnormalized;
+  let porcentaje = division * 100;
+  return porcentaje;
 }
 //HTTP REQUESTS
 
-const server  = http.listen(PORT, ()=>{
-  console.log(`Servidor levantado en ${PORT}`)
-})
-app.set("view engine", "ejs")  
-app.use(express.static(__dirname + "/public"))
+const server = http.listen(PORT, () => {
+  console.log(`Servidor levantado en ${PORT}`);
+});
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index");
+});
 
-app.get("/", (req,res)=>{
-    res.render(__dirname + "/views/index.ejs")
-})
-app.post("/producto", async (req, res)=>{
-  const {title, price, thumbnail} = req.body;
-  try{
-    await contenedorProductos.agregarObj({title, price, thumbnail, timestamp: Date.now()})
-    res.send(await contenedorProductos.obtenerAll())
-  }catch(e){
-    console.log(e)
+app.get("/api/productos-test", async (req, res) => {
+  try {
+    for (let i = 0; i < 5; i++) {
+      let product = {
+        name: faker.commerce.product(),
+        price: faker.commerce.price(),
+        thumbnail: faker.image.imageUrl(),
+      };
+      productos.push(product);
+    }
+  } catch (e) {
+    console.log(e);
   }
-})
-app.post("/mensaje", async (req, res)=>{
-  const {user, mensaje} = req.body;
-  try{
-    await contenedorMensajes.agregarObj({user, mensaje})
-    res.send(await contenedorMensajes.obtenerAll())
-  }catch(e){
-    console.log(e)
+  res.send(productos);
+  productos = [];
+});
+app.post("/api/mensaje", async (req, res) => {
+  const mensajes = req.body;
+  const newMensajes = { id: "mensajes", mensajes };
+  const normalizado = normalize(newMensajes, msjSchema);
+  const denormalizado = denormalize(
+    normalizado.result,
+    msjSchema,
+    normalizado.entities
+  );
+  db.Message.create(normalizado);
+  res.send(db.Message.find({}));
+  io.sockets.emit("messageNew", normalizado);
+  print(normalizado);
+  print(denormalizado);
+  console.log(`Normalizado : ${JSON.stringify(normalizado).length}`);
+  console.log(`Desnormalizado : ${JSON.stringify(denormalizado).length}`);
+  console.log(
+    `Porcentaje disminuido: ${compression(
+      JSON.stringify(denormalizado).length,
+      JSON.stringify(normalizado).length
+    )}%`
+  );
+});
+app.get("/api/mensaje", async (req, res) => {
+  try {
+    const mensajes = await db.Message.find({});
+    res.send(mensajes);
+  } catch (e) {
+    console.log(e);
   }
-})
-app.get("/producto", async (req,res)=>{
-  const productos  =  await contenedorProductos.obtenerAll();
-  res.send(await productos)
-})
-app.get("/mensaje", async (req, res)=>{
-  const mensajes = await contenedorMensajes.obtenerAll();
-  res.send(await mensajes)
-})
-  //SOCKETS
-  io.on("connection", function (socket){
-    console.log("Usuario conectado")
-    defaultData(socket)
-    socket.on("new-product", async (data)=>{
-      const {title, price, thumbnail} = data;
-      const {productos} =  axios.post("http://localhost:8080/producto", 
-      {title, price, thumbnail})
-      console.log(productos)
-      io.sockets.emit("productosNew", {productos})
-    })
-    socket.on("new-message", async (data)=>{
-      const {user, mensaje} = data;
-      const {mensajes} = await axios.post("http://localhost:8080/mensaje" ,
-      {user, mensaje})
-      console.log(mensajes)
-      io.sockets.emit("messageNew", {mensajes})
-    })
-  })
+});
+//Normalizr Schemas
+const authorSchema = new schema.Entity("author", {}, { idAttribute: "email" });
+const msjSchema = new schema.Entity("msj", {
+  mensajes: [{ author: authorSchema }],
+});
+//SOCKETS
+io.on("connection", async function (socket) {
+  console.log("Usuario conectado");
+  io.sockets.emit("initialProducts", await getDataProducts());
+  if (mensajes) {
+    io.sockets.emit("initialMessages", await getDataMessages());
+  }
+  console.log(db);
+  socket.on("new-message", (data) => {
+    try {
+      const { productos } = axios.post(
+        "http://localhost:8080/api/mensaje",
+        data
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  //Connection error
+  socket.on("connect_error", (err) => {
+    console.log(err.message);
+  });
+});
